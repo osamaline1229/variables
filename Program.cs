e281362cc47380e59e448440289bbcc7ab1ec871
osamaline1229 <osama98138139@gmail.com>
2023-06-05 08:18:14 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the value of  factorial : ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int i;
+        int k;
+        
+        
+        do
+        {
+            
+            a = a - 1;
+            i = a;
+            
+            i = a - 1;
+            int k = a * i;
+           
+        } while (a > 0);
+        Console.WriteLine(k);
+    }
+}
